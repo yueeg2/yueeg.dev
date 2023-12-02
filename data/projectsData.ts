756f80b36ea561fa8b0664e3ec61904e3e2d5d25
yue <yuesu198@gmail.com>
2023-12-02 18:37:28 +0800

    first commit

---
+++ data/projectsData.ts
@@ -0,0 +1,17 @@
+const projectsData = [
+  {
+    title: 'React Table',
+    description: `Custom components. Powerful table that is 
+    tabbed, sortable, collapsible, pageable, searchable and selectable.`,
+    imgSrc: '/static/images/react-table.png',
+    href: 'https://github.com/yueeg2/react-table',
+  },
+  {
+    title: 'Next App',
+    description: `An application that using next.js`,
+    imgSrc: '/static/images/next-app.png',
+    href: 'https://github.com/yueeg2/next-app',
+  },
+]
+
+export default projectsData
