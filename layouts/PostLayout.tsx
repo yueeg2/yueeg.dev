aaea7aeb9230e69b9fac03dd5ebdd2a6974a17aa
yue <yuesu198@gmail.com>
2023-12-05 00:59:34 +0800

    fix: route url

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -150,7 +150,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
               </div>
               <div className="pt-4 xl:pt-8">
                 <Link
-                  href={`/${basePath}`}
+                  href={`/`}
                   className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                   aria-label="Back to the blog"
                 >
